8a78887f53da9b8065adb1bb184acaa5ac0324d0
工业聚 <guyingjie129@163.com>
2018-05-08 19:37:26 +0800

    add isSource

--- src/sink.js
+++ src/sink.js
@@ -1,12 +1,13 @@
 import { mapValue, makeSink } from './utility'
 
-export const fork = theSink => source => sink => {
-  let finalSink = mapValue(makeSink(sink), (fn, key) => value => {
-    theSink[key](value)
-    fn(value)
+export const fork = theSink => source =>
+  source.make(sink => {
+    let finalSink = mapValue(makeSink(sink), (fn, key) => value => {
+      theSink[key](value)
+      fn(value)
+    })
+    return source(finalSink)
   })
-  return source(finalSink)
-}
 
 export const toAction = sink => source => source(makeSink(sink))
 
@@ -17,15 +18,16 @@ export const run = sink => source => {
   return action
 }
 
-export const on = name => f => source => sink => {
-  return source({
-    ...sink,
-    [name]: value => {
-      f(value)
-      sink[name](value)
-    }
+export const on = name => f => source =>
+  source.make(sink => {
+    return source({
+      ...sink,
+      [name]: value => {
+        f(value)
+        sink[name](value)
+      }
+    })
   })
-}
 
 export const onStart = on('start')
 export const onNext = on('next')
--- src/utility.js
+++ src/utility.js
@@ -3,94 +3,93 @@ export const noop = () => {}
 export const identity = x => x
 export const constant = value => () => value
 
-export const isObject = obj => Object.prototype.toString.call(obj) === '[object Object]'
-
-export const isSource = obj => !!(obj && obj.isSource)
-export const isAction = obj => !!(obj && obj.isAction)
+export const isObject = obj =>
+  Object.prototype.toString.call(obj) === '[object Object]'
+export const isSource = obj => !!(obj && typeof obj.make === 'function')
 
 const defaultSink = {
-	start: noop,
-	next: noop,
-	finish: noop,
-	error: noop
+  start: noop,
+  next: noop,
+  finish: noop,
+  error: noop
 }
 export const makeSink = (sink = {}) => {
-	return mapValue(defaultSink, (fn, key) => sink[key] || fn)
+  return mapValue(defaultSink, (fn, key) => sink[key] || fn)
 }
 
 const defaultHandler = {
-	start: noop,
-	next: noop,
-	finish: noop,
-	error: noop
+  start: noop,
+  next: noop,
+  finish: noop,
+  error: noop
 }
 
 export const makeHandler = (handler = {}) => {
-	return mapValue(defaultHandler, (fn, key) => handler[key] || fn)
+  return mapValue(defaultHandler, (fn, key) => handler[key] || fn)
 }
 
 export const mapValue = (obj, f) =>
-	Object.keys(obj).reduce((result, key) => {
-		result[key] = f(obj[key], key)
-		return result
-	}, {})
+  Object.keys(obj).reduce((result, key) => {
+    result[key] = f(obj[key], key)
+    return result
+  }, {})
 
 export const log = name => source => sink =>
-	source({
-		start: () => {
-			console.log(name, 'start')
-			sink.start()
-		},
-		next: value => {
-			console.log(name, 'next', value)
-			sink.next(value)
-		},
-		finish: () => {
-			console.log(name, 'finish')
-			sink.finish()
-		},
-		error: error => {
-			console.log(name, 'error', error)
-			sink.error(error)
-		}
-	})
+  source({
+    start: () => {
+      console.log(name, 'start')
+      sink.start()
+    },
+    next: value => {
+      console.log(name, 'next', value)
+      sink.next(value)
+    },
+    finish: () => {
+      console.log(name, 'finish')
+      sink.finish()
+    },
+    error: error => {
+      console.log(name, 'error', error)
+      sink.error(error)
+    }
+  })
 
 export const logValue = name => source => sink =>
-	source({
-		...sink,
-		next: value => {
-			console.log(name, value)
-			sink.next(value)
-		}
-	})
+  source({
+    ...sink,
+    next: value => {
+      console.log(name, value)
+      sink.next(value)
+    }
+  })
 
 export const logAll = name => source => sink => {
-	let action = source({
-		start: () => {
-			console.log(name, 'sink:start')
-			sink.start()
-		},
-		next: value => {
-			console.log(name, 'sink:next', value)
-			sink.next(value)
-		},
-		finish: () => {
-			console.log(name, 'sink:finish')
-			sink.finish()
-		},
-		error: error => {
-			console.log(name, 'sink:error', error)
-			sink.error(error)
-		}
-	})
-	return {
-		start: () => {
-			console.log(name, 'action:start')
-			action.start()
-		},
-		finish: () => {
-			console.log(name, 'action:finish')
-			action.finish()
-		}
-	}
+  let action = source({
+    start: () => {
+      console.log(name, 'sink:start')
+      sink.start()
+    },
+    next: value => {
+      console.log(name, 'sink:next', value)
+      sink.next(value)
+    },
+    finish: () => {
+      console.log(name, 'sink:finish')
+      sink.finish()
+    },
+    error: error => {
+      console.log(name, 'sink:error', error)
+      sink.error(error)
+    }
+  })
+  return {
+    start: () => {
+      console.log(name, 'action:start')
+      action.start()
+    },
+    finish: () => {
+      console.log(name, 'action:finish')
+      action.finish()
+    }
+  }
 }
